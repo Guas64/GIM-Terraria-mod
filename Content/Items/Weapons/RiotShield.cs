c77351c44eb6cb65b87ffa804170da949678ab22
Mediocre Pancake <115119016+MediocrePancake@users.noreply.github.com>
2022-10-05 20:52:43 -0600

    Add files via upload

---
+++ Content/Items/Weapons/RiotShield.cs
@@ -0,0 +1,51 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace GIMTestModMedPan.Items.Weapons
+{
+    internal class RiotShield : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Riot Shield");
+            Tooltip.SetDefault("Knocks enemies back\nSlows you down");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 36;
+            Item.rare = ItemRarityID.Blue;
+            Item.maxStack = 1;
+            Item.knockBack = 10;
+            Item.damage = 10;
+            Item.crit = 5;
+            Item.useTime = 4;
+            Item.useAnimation = 4;
+            Item.DamageType = DamageClass.MeleeNoSpeed;
+            Item.useStyle = ItemUseStyleID.Guitar;
+            Item.autoReuse = true;
+            Item.shootSpeed = 8f;
+        }
+        public override bool? UseItem(Player player)
+        {
+            player.AddBuff(BuffID.Slow,10);
+
+
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.HallowedBar, 12)
+                .AddIngredient(ItemID.LaserRifle)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
