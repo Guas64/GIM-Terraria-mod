367bea9ad936be3dbe947e8fe22e4fc3a7e052d7
Mediocre Pancake <115119016+MediocrePancake@users.noreply.github.com>
2022-10-06 12:35:00 -0600

    Update SpectreFlower.cs

--- Content/Items/Accessories/SpectreFlower.cs
+++ Content/Items/Accessories/SpectreFlower.cs
@@ -3,7 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.GameContent.Creative;
 
-namespace GIMTestModMedPan.Items.Accessories
+namespace gim.Content.Items.Accessories
 {
     internal class SpectreFlower : ModItem
     {
